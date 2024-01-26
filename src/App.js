7927dc829a94c76332fc3e63ea73f6338aeb06d6
minnica <minnica20@gmail.com>
2024-01-26 01:32:25 -0600

    add ShowBlogs.js and table with buttons in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+// importamos los componentes
+import CompShowBlogs from './blog/ShowBlogs';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className='btn btn-primary'><i className="fa-brands fa-react"></i></button>
       </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <CompShowBlogs></CompShowBlogs> }></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/blog/ShowBlogs.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const URI = 'http://localhost:8000/blogs'
+
+const CompShowBlogs = () => {
+  const [blogs, setBlog] = useState([])
+  useEffect(() => {
+    getBlogs()
+  }, [])
+
+  // procedimiento para mostrar todos los blogs
+  const getBlogs = async () => {
+    const res = await axios.get(URI)
+    setBlog(res.data)
+  }
+
+  // procedimiento para eliminar un blog
+  const deleteBlog = async (id) => {
+    axios.delete(`${URI} ${id}`)
+    getBlogs()
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+
+        <div className="col">
+          <Link to={'/create'} className='btn btn-primary mt-2 mb-2'>Create</Link>
+          <table className="table">
+            <thead className="table-primary">
+              <tr>
+                <th>Title</th>
+                <th>Content</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {blogs.map((blog) => (
+                <tr key={blog.id}>
+                  <td> {blog.title} </td>
+                  <td> {blog.content} </td>
+                  <td>
+                    <Link to={`/edit/${blog.id}`} className="btn btn-info" >Edit</Link>
+                    <button onClick={() => deleteBlog(blog.id)} className="btn btn-danger">Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default CompShowBlogs
