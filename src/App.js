b4ad23f7999a0108b03a77f4b6b48bffb64a22bb
minnica <minnica20@gmail.com>
2024-01-26 00:21:13 -0600

    import bootstrap and fontawesome and new pages in blogs

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <button className='btn btn-primary'><i className="fa-brands fa-react"></i></button>
       </header>
     </div>
   );
